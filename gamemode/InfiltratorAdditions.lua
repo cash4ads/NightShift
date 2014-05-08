769eb023293ed4ea768c93d257c6d1821a387d97
Igotaquestion <marcusapatino@gmail.com>
2014-05-08 00:19:10 -0400

    Added Infiltrator file
    For some reason it didnt do it on Saturday

---
+++ gamemode/InfiltratorAdditions.lua
@@ -0,0 +1,20 @@
+--Infiltrator disguise method
+function disguise()
+	local ply = LocalPlayer()
+	local trace = util.GetPlayerTrace( ply )
+	local traceRes = util.TraceLine( trace )
+	if traceRes.HitNonWorld then
+		local target = traceRes.Entity
+		if target:IsPlayer() then
+			local targetModel = target:GetModel
+			ply:SetModel(targetModel)
+		end
+	end
+end
+function KeyPressed(P,key)
+	if(P:GetName == IN_ATTACK2) then
+		disguise()
+	end 
+end
+
+hook.Add("KeyPressed","KeyPressedHook",KeyPressed)
