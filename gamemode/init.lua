5128cb4bdfadc7a99f0f9a5c19a9a03ebc6dca55
Igotaquestion <marcusapatino@gmail.com>
2014-05-14 21:26:38 -0400

    fixed assignorders

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -48,23 +48,11 @@ function PrintSetupEnd()
 
 function assignOrders( orders )
 
-switch[1] = "ENT1"
-switch[2] = "ENT2"
-switch[3] = "ENT3"
-switch[4] = "ENT4"
-switch[5] = "ENT5"
-switch[6] = "ENT6"
-switch[7] = "ENT7"
-switch[8] = "ENT8"
-switch[9] = "ENT9"
-switch[10] = "ENT10"
-switch[11] = "ENT11"
-switch[12] = "ENT12"
-	 
-
-  	if switch[orders] then 
+	if math.abs(orders - 6) < 6 then	
+  		if switch["ENT"...orders] then 
     	
-  	end
+  		end
+	end 
 end
 timer.Create("OrderAssignment", 90, 0, assignOrders)
 end
