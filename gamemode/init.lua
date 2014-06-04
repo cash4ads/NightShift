09a53a61f77059bbf073b055a7832a8ef4284e48
Conro101 <cbot.596@gmail.com>
2014-06-03 23:25:43 -0400

    the map

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -45,12 +45,33 @@ function PrintSetupEnd()
 	for k, ply in pairs(player.GetAll()) do
 		ply:ChatPrint("Setup has ended.")
 	end
+	
+--Infiltrator disguise method
+function disguise()
+	local ply = LocalPlayer()
+	local trace = util.GetPlayerTrace( ply )
+	local traceRes = util.TraceLine( trace )
+	if traceRes.HitNonWorld then
+		local target = traceRes.Entity
+		if target:IsPlayer() then
+			local targetModel = target:GetModel
+			ply:SetModel(targetModel)
+		end
+	end
+end
+function KeyPressed(P,key)
+	if(key == IN_ATTACK2) then
+		disguise()
+	end 
+end
+
+hook.Add("KeyPress","KeyPressHook",KeyPress)
 
 function assignOrders( orders )
 
 	if math.abs(orders - 6) < 6 then	
   		if switch["ENT"...orders] then 
-    	
+    		
   		end
 	end 
 end
